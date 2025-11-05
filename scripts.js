7498b92214bc75a93fbe68a7cf952a1854210683
Daniela Spencer <danielaspencer9@gmail.com>
2025-11-05 15:49:53 -0400

    completed landing page

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 /* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('form');
+  const emailInput = document.getElementById('email');
+  const messageDiv = document.querySelector('.message');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const emailValue = emailInput.value.trim();
+
+    if (emailValue !== '') {
+      messageDiv.textContent = `Thank you! Your email address ${emailValue} has been added to our mailing list!`;
+    } else {
+      messageDiv.textContent = 'Please enter a valid email address';
+    }
+  });
+});
